79c375bacc3f5cc71c516465ff0582cf2f1ee6bc
andaleeb99 <andaleeb.zghoul@gmail.com>
2023-06-09 18:56:24 +0300

    new

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 alert("hello")
+
+
+let mystudents = ["ahmad" , "ali", "mustafa"]
+let mystudentsmarks = [90, 50, 33]
+function markscalculate(params) {
+
+
+    for (let i = 0; i < mystudentsmarks.length; i++) {
+        if (mystudentsmarks[i] <= 49) {
+            console.log ("sorry " + mystudents[i] + "you scored " + mystudentsmarks[i] + " and you have failed ");
+        }
+else if ( mystudentsmarks [i] >= 50) {  console.log ( "congrats "  + mystudents[i] + " you scored " + mystudentsmarks[i] + " and you have passed ");}
+    }
+}
+
+markscalculate();
+
+
+
+
+
+
